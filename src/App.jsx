386db3e0e1b6f138cdab30e0e5dd7e534126eddd
Anandhakrishnan <anandhakrishnanvellat15@gmail.com>
2025-01-15 16:06:24 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router";
+import Navbar from "./Components/NavBar/Navbar";
+import Home from "./Components/Pages/Homepage/Home";
+import AboutUs from "./Components/Pages/AboutUsPage/AboutUs";
+import ContactUs from "./Components/Pages/ContactUsPage/ContactUs";
+import Resources from "./Components/Pages/ResourcesPage/Resources";
+import Products from "./Components/Pages/ProductPage/Products";
+import ProductDetails from "./Components/Pages/ProductPage/ProductDetails";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About-us" element={<AboutUs />} />
+        <Route path="/Contact-us" element={<ContactUs />} />
+        <Route path="/Resources" element={<Resources />} />
+        <Route path="/Products" element={<Products />} />
+        {/* Product-specific routes */}
+        <Route path="/Products/:productId" element={<ProductDetails />} />
+        <Route
+          path="/Products/:productId/:subProductId"
+          element={<ProductDetails />}
+        />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
