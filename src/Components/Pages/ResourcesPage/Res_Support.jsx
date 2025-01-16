e1387d056b19be856c2a903b195e6e5f298da72a
Anandhakrishnan <anandhakrishnanvellat15@gmail.com>
2025-01-16 22:16:35 +0530

    resources and support  heading

---
+++ src/Components/Pages/ResourcesPage/Res_Support.jsx
@@ -0,0 +1,7 @@
+const Res_Support = [
+  { id: "powercalcualtion", name: "powercalcualtion" },
+  { id: "application", name: "application" },
+  { id: "download", name: "download" },
+];
+
+export default Res_Support
